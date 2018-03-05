91f257806f5b67ecf92b2776fcecef53b236a512
spkuehl <skuehlhorn@gmail.com>
2018-03-05 17:47:14 -0600

    Converted all strings to .format(), added user input to exit script.

--- stateCity/main.py
+++ stateCity/main.py
@@ -2,23 +2,25 @@
 create a program that returns both largest city and capital based on user input
  for state name or state abbreviation.
 '''
-from stateCity.states import States
+from states import States
 
 states = States()
 
 if __name__ == '__main__':
-    print('please enter State Name or Abbreviation you wish to know the largest city and capital')
+    print('Please enter State Name or Abbreviation you wish to know the largest city and capital.')
 
     while True:
-        state_input = input('What state: ')
-        if len(state_input) == 2:
+        state_input = input('What state? ("q" to quit): ')
+        if state_input.lower() == 'q':
+            exit()
+        elif len(state_input) == 2:
             state = states.by_abbr(state_input)
         else:
             state = states.by_name(state_input)
-        print('entered: ', state_input)
+        print('Entered: {}'.format(state_input))
         if state:
             print('In State of {}'.format(state.name))
-            print('Largest City:', state.largest_city)
-            print('Capital:', state.capital)
+            print('Largest City: {}'.format(state.largest_city))
+            print('Capital: {}'.format(state.capital))
         else:
-            print('bad input')
+            print('Bad input, try again. ("q" to quit): ')
